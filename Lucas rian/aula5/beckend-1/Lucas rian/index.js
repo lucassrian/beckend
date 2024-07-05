45ac22471cea3b8d81e7bcde567596cd9145170e
Lucas rian <165908139+lucassrian@users.noreply.github.com>
2024-07-05 11:56:43 -0300

    Add files via upload

---
+++ Lucas rian/aula5/beckend-1/Lucas rian/index.js
@@ -0,0 +1,46 @@
+/*let nome = 'Lucas'
+let sobrenome = 'Sousa'
+let idade = 29
+let estudante = true
+console.log(`Meu nome é ${nome} ${sobrenome}, e tenho ${idade} anos, se eu sou estudante? ${estudante}`)*/
+
+/*const num1 = '50'
+const num2 = '70'
+const newNum1 = Number(num1)
+const newNum2 = Number(num2)
+const soma = newNum1 + newNum2
+console.log(soma)*/
+ /*const readlineSync = require('readline-sync');
+
+ const num1 = Number(readlineSync.question('Insira um número'))
+ const num2 = Number(readlineSync.question('Insira outro número'))
+
+ console.log( num1 + num2)*/
+ /*const readlineSync = require('readline-sync');
+
+ const num1 = Number(readlineSync.question('Insira um numero'))
+ const num2 = Number(readlineSync.question('Insira um segundo numero'))
+ const num3 = Number(readlineSync.question('Insira um terceiro numero'))
+
+ console.log( (num1 + num2 + num3) / 3)*/
+ 
+
+
+ /*const readlineSync = require ('readline-sync');
+ 
+
+ const valor1 = readlineSync.question('Insira um valor')
+ const valor2 = readlineSync.question('Insira um segundo valor')
+
+ console.log('O primeiro valor e igual o segundo valor?',valor1 === valor2)
+ console.log('O primeiro valor e diferente do segundo valor?',valor1 !== valor2)
+ console.log('O primeiro valor e maior que o segundo valor?',valor1 > valor2)
+ console.log('O primeiro valor e maior  igual o segundo valor?',valor1 >= valor2)
+ console.log('O primeiro valor e menor que  o segundo valor?',valor1 < valor2)
+ console.log('O primeiro valor e menor igual que o segundo valor?',valor1 <= valor2)*/
+
+/*const readlineSync = require ('readline-sync')
+const pergunta = Number(readlineSync.question('digete um numero de 0 a 2:'))
+const raças = [Beagle, Dalmata, PastorAlemão]
+
+console.log('Sua raça é', pergunta[raças])*/
