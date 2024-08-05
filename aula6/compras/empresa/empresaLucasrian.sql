c84d306e93c87ed768a341eb8b243c2c3b3db8b8
Lucas rian <165908139+lucassrian@users.noreply.github.com>
2024-08-05 09:14:22 -0300

    Add files via upload

---
+++ aula6/compras/empresa/empresaLucasrian.sql
@@ -0,0 +1,172 @@
+CREATE TABLE departamentos(
+    departamento_id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL
+    );
+     CREATE TABLE empregados(
+         empregado_id INT AUTO_INCREMENT PRIMARY KEY,
+         nome VARCHAR(50) NOT NULL,
+         salario DECIMAL(10,2) NOT NULL,
+         departamento_id INT,
+         FOREIGN KEY (departamento_id) REFERENCES departamentos(departamento_id)
+         ); 
+
+
+INSERT INTO departamentos(nome) VALUES
+('Vendas')
+('TI')
+('Markting')
+('Recursos Humanos')
+
+INSERT INTO empregados(nome, salario,departamento_id) VALUES
+('Robson', 5000, 1)
+('Cleusa', 3000, 1)
+('Cleitom', 5000, 2)
+('Dewid', 6000, 3)
+('Vanesca', 7000, 3)
+('Vanderleya', 5000, 4)
+('Robinho', 9000, 3)
+('Valdirene', 4000, 4)
+('rodnei', 3500, 2)
+
+SELECT nome, salario
+FROM empregados 
+WHERE salario > (SELECT AVG(salario)FROM empregados);
+
+ALTER TABLE departamentos
+ADD localizacao VARCHAR(80)
+
+UPDATE departamentos 
+SET localizacao = 'São Paulo' 
+WHERE nome = 'Vendas';
+
+UPDATE departamentos 
+SET localizacao = 'Rio de Janeiro' 
+WHERE nome = 'Marketing';
+
+UPDATE departamentos
+SET localizacao = 'São Paulo' 
+WHERE nome = 'TI';
+
+UPDATE departamentos 
+SET localizacao = 'Belo Horizonte' 
+WHERE nome = 'Recursos Humanos';
+
+SELECT nome
+FROM empregados
+WHERE departamento_id IN (SELECT departamento_id FROM departamentos WHERE localizacao = 'São Paulo');
+
+
+ Exemplo com ANY
+
+Exemplo: Salário Maior que Qualquer Salário no Departamento de TI
+
+SELECT nome, salario
+FROM empregados
+WHERE salario > ANY (SELECT salario FROM empregados WHERE departamento_id = (SELECT departamento_id FROM departamentos WHERE nome = 'TI'));
+
+SELECT nome, salario
+FROM empregados
+WHERE salario > ALL (SELECT salario FROM empregados WHERE departamento_id = (SELECT id FROM departamentos WHERE nome = 'TI'));
+
+ALTER TABLE empregados ADD titulo VARCHAR(50);
+
+UPDATE empregados SET titulo = 'Gerente' WHERE nome = 'Robinho';
+UPDATE empregados SET titulo = 'Assistente' WHERE nome = 'rodnei';
+UPDATE empregados SET titulo = 'Analista' WHERE nome = 'Dewid';
+UPDATE empregados SET titulo = 'Desenvolvedor' WHERE nome = 'Vanesca';
+UPDATE empregados SET titulo = 'Engenheiro' WHERE nome = 'Robson';
+UPDATE empregados SET titulo = 'Assistente' WHERE nome = 'Vanderleya';
+UPDATE empregados SET titulo = 'Analista' WHERE nome = 'Valdirene';
+UPDATE empregados SET titulo = 'Estagiário' WHERE nome = 'Cleusa';
+UPDATE empregados SET titulo = 'Analista' WHERE nome = 'Cleitom';
+
+
+INSERT INTO cargos (titulo, departamento_id, salario) VALUES
+('Gerente', 1, 8000.00),
+('Assistente', 1, 4000.00),
+('Analista', 2, 4500.00),
+('Desenvolvedor', 3, 6000.00),
+('Engenheiro', 3, 7000.00),
+('Assistente', 4, 3500.00),
+('Analista', 2, 4600.00),
+('Estagiário', 1, 2000.00);
+
+SELECT departamentos.nome, AVG(cargos.salario)
+FROM empregados
+JOIN cargos ON empregados.departamento_id = cargos.departamento_id
+JOIN departamentos ON empregados.departamento_id = departamentos.departamento_id
+GROUP BY departamentos.nome
+HAVING AVG(cargos.salario) > 5000.00;
+
+--1
+SELECT departamentos.nome, sum(cargos.salario)
+FROM empregados
+JOIN cargos ON empregados.departamento_id = cargos.departamento_id
+JOIN departamentos ON empregados.departamento_id = departamentos.departamento_id
+GROUP BY departamentos.nome
+HAVING sum(cargos.salario) > 12000.00;
+
+--2
+SELECT cargos.titulo, count(empregados.titulo)
+FROM empregados
+JOIN cargos ON empregados.departamento_id = cargos.departamento_id
+JOIN departamentos ON empregados.departamento_id = departamentos.departamento_id
+GROUP BY departamentos.nome
+HAVING count(empregados.titulo) >= 2;
+
+--3
+SELECT departamentos.nome, AVG(cargos.salario)
+FROM empregados
+JOIN cargos ON empregados.departamento_id = cargos.departamento_id
+JOIN departamentos ON empregados.departamento_id = departamentos.departamento_id
+GROUP BY departamentos.nome
+HAVING AVG(cargos.salario) > 4500.00;
+
+--4
+SELECT departamentos.nome, de max(cargos.salario)
+FROM empregados
+JOIN cargos ON empregados.departamento_id = cargos.departamento_id
+JOIN departamentos ON empregados.departamento_id = departamentos.departamento_id
+GROUP BY departamentos.nome
+HAVING max(cargos.salario) > 6000.00;
+
+--5
+SELECT departamentos.nome, min(cargos.salario)
+FROM empregados
+JOIN cargos ON empregados.departamento_id = cargos.departamento_id
+JOIN departamentos ON empregados.departamento_id = departamentos.departamento_id
+GROUP BY departamentos.nome
+HAVING min(cargos.salario) > 2500.00;
+
+CREATE PROCEDURE AumentarSalario(
+    IN dep_id INT,
+    IN percentual DECIMAL(5,2)
+)
+BEGIN
+    UPDATE cargos 
+    SET salario = salario + (salario * (percentual / 100))
+    WHERE departamento_id = dep_id;
+END 
+
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DimunuirSalario(IN dep_id INT,IN desconto DECIMAL(5,2))
+BEGIN
+    UPDATE cargos 
+    SET salario = salario + desconto 
+    WHERE departamento_id = dep_id;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE DimunuirSalario(IN dep_id INT,IN desconto DECIMAL(5,2))
+BEGIN
+    UPDATE cargos 
+    SET salario = salario + desconto 
+    WHERE departamento_id = dep_id;
+END //
+
+DELIMITER ;
---
+++ aula6/compras/empresa/index.js
@@ -0,0 +1,56 @@
+/*let nome:string = 'lucas'
+
+let idade:number  = 16
+
+let souEstudante:boolean  = true
+console.log(`Meu nome é ${nome} e tenho ${idade} anos. Sou estudante?${souEstudante}`)*/
+/*import * as readlineSync from 'readline-sync';
+
+let nome:string =(readlineSync.question('Informe o seu nome:'))
+
+let idade:number  = readlineSync.question('Informe a sua idade:')
+
+let temCarteira:string  = readlineSync.question('Você tem carteira:')
+if
+
+function podeDirigir (nome:string, idade:number, temCarteira:string):void{
+    if ( idade > 18 && temCarteira === 'sim' ){
+        console.log('Pode dirigir!!');
+    } else{
+        console.log('Não pode dirigir');
+}
+        
+}
+podeDirigir( nome, idade, temCarteira)*/
+// ARRAY
+/*let arr:Array<number> - [1, 2, 3, 4]
+
+//ou
+
+let array:number[]= [1, 2, 3, 4]
+
+// OBJETOS
+
+let person:(name:string, age:number)= {
+    name:'luacas'
+    age:16
+}
+
+// FUNCTION
+
+function soma(num1:number, num2:number):number{
+    return num1+num2
+}
+
+
+function frase(nome:string)string{
+    return `Olá ${nome}, tudo bem?`
+}
+
+function frase(name?:string)void{
+    console.log("hello", name || "world")
+
+    // o "?" faz com que se não imprimir o "name" ele imprima "world"
+
+}*/
+// Molde para objetos tipo cachorro
